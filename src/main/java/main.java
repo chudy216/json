9540b905090a4f2c72f68806dfd1c02563d0e3f7
chudy216 <kam.kwi.02@wp.pl>
2021-04-29 15:32:31 +0200

    podział na klasy i menu epickie

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,6 +2,8 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
         try {
@@ -12,8 +14,8 @@ public class main {
                 throw new RuntimeException("Błąd" + odpowiedz.getStatus());
             }
             String pobrany = odpowiedz.getEntity(String.class);
-            System.out.println(pobrany);
             String [] actualValue = pobrany.split("\"");
+            System.out.println(actualValue[11]+" ~"+actualValue[7]);
             String link = ("https://api.jikan.moe/v3/search/anime?q="+actualValue[3]);
             if (link.contains(" ")) {
                 String[] spacje = link.split(" ");
@@ -26,35 +28,14 @@ public class main {
                 }
 
             }
-            Client klient2 = Client.create();
-            WebResource webzrodlo2 = klient2.resource(link);
-            ClientResponse odpowiedz2 = webzrodlo2.accept("application/json").get(ClientResponse.class);
-            if (odpowiedz2.getStatus() != 200) {
-                throw new RuntimeException("Błąd" + odpowiedz2.getStatus());
-            }
-            String pobrany2 = odpowiedz2.getEntity(String.class);
-            String [] actualValue1 = pobrany2.split("\"");
-            String mal = (actualValue1[15]);
-            String img = (actualValue1[19]);
-            String [] mal1 = mal.split("\\\\");
-            String [] img1 = img.split("\\\\");
-            int im = 1;
-            int ii = 1;
-            String mal2 = mal1[0];
-            String img2 = img1[0];
-            int maldlugosc = mal1.length;
-            int imgdlugosc = img1.length;
-            while (maldlugosc>im) {
-                mal2 = mal2+mal1[im];
-                im++;
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("1. End");
+            System.out.println("2. Get link to mal");
+            System.out.println("3. Get anime image");
+            System.out.println("4. Get link to mal and image");
+            String wyborek = myObj.nextLine();
+            new api2(link, wyborek);
 
-            }
-            while (imgdlugosc>ii) {
-                img2 = img2+img1[ii];
-                ii++;
-            }
-            System.out.println(img2);
-            System.out.println(mal2);
 
         } catch (Exception e) {
             e.printStackTrace();
